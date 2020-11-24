3362a0ddd8e3ff9d8b882360578abc72a9fe6bf3
yimoljt <74773646+yimoljt@users.noreply.github.com>
2020-11-24 20:47:03 +0800

    Create 112402.App.java

---
+++ 112402.App.java
@@ -0,0 +1,51 @@
+package pac;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class App {
+    JPanel myPanel=new JPanel();
+    JPanel label_tet=new JPanel();
+    JTextArea textArea_A=new JTextArea();
+    JTextArea textArea_B=new JTextArea();
+    //构造方法
+    public App() {
+        textArea_A.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                if (textArea_A.getText().equals("你真棒")) {
+                    textArea_B.setText("输入正确\n");
+                }
+                else {
+                    textArea_B.setText("输入错误\n");
+                }
+            }
+        });
+    }
+    //显示窗体方法
+    void go(){
+        textArea_A.setBounds(100,100,300,150);
+        textArea_A.setBackground(Color.pink);
+        myPanel.add(textArea_A);
+        textArea_B.setBounds(100,300,300,150);
+        textArea_B.setBackground(Color.ORANGE);
+        myPanel.add(textArea_B);
+
+
+        JFrame frame = new JFrame("TestGUI");
+        myPanel.setLayout(null);
+        myPanel.add(label_tet);
+        frame.setContentPane(myPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(100,100,512,720);
+        //frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new App().go();
+    }
+}
